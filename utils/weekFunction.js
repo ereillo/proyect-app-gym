800deab6845e672067b6dd53748e301be943ee21
ereillo <evelioreillo@gmail.com>
2023-08-17 20:34:10 +0200

    solved

--- utils/weekFunction.js
+++ utils/weekFunction.js
@@ -23,18 +23,5 @@ function getWeekDetails() {
 }
 
 
-// const  weekFunction = Week.findById(
-//     "64da35b47a1247b56b3042b4"
-//   ).populate({
-//     path: "monday tuesday wednesday thursday friday",
-//     populate: {
-//       path: "at9 at12 at15 at18",
-//       model: "Class",
-//       populate: {
-//         path: "teacher students",
-//         model: "User",
-//       },
-//     },
-//   });
 
   module.exports = getWeekDetails
